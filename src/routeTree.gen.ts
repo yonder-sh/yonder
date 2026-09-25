be6868a99cf50b6864cd81fb5d0a78d25f1cf94d
Dennis Pham <dennis@dennispham.me>
2026-09-25 03:42:55 -0400

    chore: regenerate the route tree

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -500,7 +500,6 @@ export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
   ._addFileTypes<FileRouteTypes>()
 
-
 import type { getRouter } from './router.tsx'
 import type { startInstance } from './start.ts'
 declare module '@tanstack/react-start' {
